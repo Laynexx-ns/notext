4000841f6670641c7bea3099af60fc0338164c12
laynexx <argo150808@gmail.com>
2025-02-03 20:21:16 +0300

    init

---
+++ pages/register.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="flex relative  h-screen w-screen">
+   <div class="flex flex-col bg-black items-center justify-center  sm:w-2/3 text-white lg:w-1/3">
+      <span> signup for a free account</span>
+   </div>
+   <div class="flex-1">
+
+   </div>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
